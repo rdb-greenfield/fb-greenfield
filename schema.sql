d5a61c9ebad0d765ae02bf2e27f8da2547e33512
Warren <warren.p.kwong@gmail.com>
2018-04-10 14:42:35 -0700

    Schema framework created

--- schema.sql
+++ schema.sql
@@ -1,15 +1,62 @@
-DROP DATABASE IF EXISTS test;
+DROP DATABASE IF EXISTS greenfield;
 
-CREATE DATABASE test;
+CREATE DATABASE greenfield;
 
-USE test;
+USE greenfield;
 
-CREATE TABLE items (
+CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
-  quantity integer NOT NULL,
-  description varchar(50) NOT NULL,
-  PRIMARY KEY (ID)
+  firstName varchar(25) NOT NULL,
+  lastName varchar(25) NOT NULL,
+  pw varchar(25) NOT NULL,
+  profilePicture varchar(50),
+  email varchar(50),
+  aboutMe varchar(500),
+  hometown varchar(25),
+  createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE friends (
+  id int NOT NULL AUTO_INCREMENT,
+  user1_id int,
+  user2_id int,
+  active boolean,
+  FOREIGN KEY (user1_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (user2_id) REFERENCES users(id) ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE posts (
+  id int NOT NULL AUTO_INCREMENT,
+  author_id int,
+  owner_id int, 
+  createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
+  body varchar(500),
+  img varchar(50),
+  video varchar(50),
+  likes int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE comments (
+  id int NOT NULL AUTO_INCREMENT,
+  author_id int,
+  post_id int, 
+  parent_id int, 
+  createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
+  body varchar(500),
+  img varchar(50),
+  video varchar(50),
+  likes int,
+  FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+  FOREIGN KEY (parent_id) REFERENCES comments(id) ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
+
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
