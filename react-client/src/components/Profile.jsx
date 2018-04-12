3a137c6da107c8d0dc04d5bc887a3b2a622ec515
Mark Khuzam <khuzammark@gmail.com>
2018-04-12 14:09:13 -0700

    completed profile header and nav

---
+++ react-client/src/components/Profile.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <div className="profileWrapper">
+        <div className="coverPhoto">this is the cover photo</div>
+        <div className="profilePic">
+          <img
+            className="pic"
+            src="http://emblemsbattlefield.com/uploads/posts/2014/7/facebook-default-picture_1.jpg"
+            alt=""
+          />
+        </div>
+        <div className="profileNav">
+          <ul>
+            <li>
+              <a href="#">Timeline</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Friends</a>
+            </li>
+            <li>
+              <a href="#">Photos</a>
+            </li>
+            <li>
+              <a href="#">More</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
