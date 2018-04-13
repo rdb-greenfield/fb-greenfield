36b9854d2010f6bd1d152d72e3f6e08dded51e57
Warren <warren.p.kwong@gmail.com>
2018-04-12 19:59:08 -0700

    Initial layout for ProfileWall (Posts)

--- react-client/src/components/ProfileWall.jsx
+++ react-client/src/components/ProfileWall.jsx
@@ -1,7 +1,34 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class ProfileWall extends Component {
   render() {
-    return <div className="profileWall">this is the profile wall</div>;
+    return (
+      <div className="profileWall">
+        <img
+          className="post-img-circle"
+          src="http://emblemsbattlefield.com/uploads/posts/2014/7/facebook-default-picture_1.jpg"
+          alt="default photo"
+        />
+        <div className="post-header">
+          <a href="#">
+            <span>USER</span>
+          </a>
+          <br />
+          <span>DATE</span>
+        </div>
+        <div className="post-body">
+          BODY TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT
+        </div>
+        <div className="post-footer">
+          <a href="#">Like</a>
+          <a href="#">Comment</a>
+          <a href="#">Share</a>
+        </div>
+        <div className="like-counter"># Likes</div>
+        <div className="comment-feed">
+          <div>HERE BE THE COMMENTS</div>
+        </div>
+      </div>
+    );
   }
 }
