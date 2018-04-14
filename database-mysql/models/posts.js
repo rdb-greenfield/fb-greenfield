4ab3f3bd0239280551e59f9846eeca7b9a1d71a7
Tyler Standley <tyler.standley21@gmail.com>
2018-04-14 16:55:29 -0700

    Fixed sql queries

--- database-mysql/models/posts.js
+++ database-mysql/models/posts.js
@@ -11,7 +11,7 @@ const insertNewPost = (a_id, o_id, p_id, pt, b, i, v, l, callback) => {
 
 // query for all posts owned by the user (posts, comments, sub-comments)
 const selectWallPosts = (o_id, callback) => {
-  const query = `SELECT posts.author_id, posts.owner_id, posts.parent_id, posts.post_type, posts.createdat, posts.body, posts.img, posts.video, posts.likes, users.id, users.firstname, users.lastname, users.profilepicture FROM posts LEFT JOIN users ON posts.author_id=users.id WHERE posts.owner_id='${o_id}'`;
+  const query = `SELECT posts.id, posts.author_id, posts.owner_id, posts.parent_id, posts.post_type, posts.createdat, posts.body, posts.img, posts.video, posts.likes, users.firstname, users.lastname, users.profilepicture FROM posts LEFT JOIN users ON posts.author_id=users.id WHERE posts.owner_id='${o_id}'`;
   db.query(query, function(err, results, fields) {
     err ? callback(err, null) : callback(null, results);
   });
@@ -33,7 +33,7 @@ const selectFeedPosts = (o_id, callback) => {
           : friends.push(friend.user2_id);
       });
       // query for user + friends owned posts
-      const query2 = `SELECT posts.author_id, posts.owner_id, posts.parent_id, posts.post_type, posts.createdat, posts.body, posts.img, posts.video, posts.likes, users.id, users.firstname, users.lastname, users.profilepicture FROM posts LEFT JOIN users ON posts.author_id=users.id WHERE posts.owner_id IN (${[
+      const query2 = `SELECT posts.id, posts.author_id, posts.owner_id, posts.parent_id, posts.post_type, posts.createdat, posts.body, posts.img, posts.video, posts.likes, users.firstname, users.lastname, users.profilepicture FROM posts LEFT JOIN users ON posts.author_id=users.id WHERE posts.owner_id IN (${[
         ...friends
       ]})`;
       db.query(query2, function(err, results, fields) {
